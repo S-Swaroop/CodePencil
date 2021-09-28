a54b13b5d411d57863e21d8676d18718eac89e35
S-Swaroop <itsmesharan2000@gmail.com>
2021-09-28 15:46:49 +0530

    Updates

---
+++ precache-manifest.b5e818aaee3fa3f5f816d3e6509e930f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31fb701d2a15b78022a9132fbcdf9487",
+    "url": "/CodePencil/index.html"
+  },
+  {
+    "revision": "b33f33e38c5305fd19bf",
+    "url": "/CodePencil/static/css/2.40796e8b.chunk.css"
+  },
+  {
+    "revision": "2be10b40526dd35e5461",
+    "url": "/CodePencil/static/css/main.82b6e4c5.chunk.css"
+  },
+  {
+    "revision": "b33f33e38c5305fd19bf",
+    "url": "/CodePencil/static/js/2.88f51c80.chunk.js"
+  },
+  {
+    "revision": "2be10b40526dd35e5461",
+    "url": "/CodePencil/static/js/main.a1f2e555.chunk.js"
+  },
+  {
+    "revision": "2c2a990189ebf79683ec",
+    "url": "/CodePencil/static/js/runtime~main.97ffaec5.js"
+  }
+]);
